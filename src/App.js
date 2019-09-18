fcd5e5f646f1e43f19ae2703239fc0726ee720e8
kat.bosnic@mccann.com <kat.bosnic@mccann.com>
2019-09-18 13:45:18 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>The New Farmer's Almanac-ish</h1>
+          <h2>Founded in 2019</h2>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
